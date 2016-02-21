8af372412a7dd03001c89df19a9800d6f7a1f78f
LeafyDev <will09600@gmail.com>
2016-02-21 18:26:30 +0100

    Init

---
+++ Leafy-MD5check/Classes/DelegateCommand.cs
@@ -0,0 +1,39 @@
+﻿// -----------------------------------------------------------
+// This program is private software, based on C# source code.
+// To sell or change credits of this software is forbidden,
+// except if someone approves it from the LeafyCoding INC. team.
+// -----------------------------------------------------------
+// Copyrights (c) 2016 Leafy-MD5check INC. All rights reserved.
+// -----------------------------------------------------------
+
+#region
+
+using System;
+using System.Windows.Input;
+
+#endregion
+
+namespace Leafy_MD5check.Classes
+{
+    internal sealed class DelegateCommand : ICommand
+    {
+        private readonly Func<bool> _canExecute;
+        private readonly Action _execute;
+
+        public DelegateCommand(Action execute, Func<bool> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter) => _canExecute == null || _canExecute();
+
+        public void Execute(object parameter) => _execute();
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
---
+++ Leafy-MD5check/Classes/SelectableViewModel.cs
@@ -0,0 +1,65 @@
+﻿// -----------------------------------------------------------
+// This program is private software, based on C# source code.
+// To sell or change credits of this software is forbidden,
+// except if someone approves it from the LeafyCoding INC. team.
+// -----------------------------------------------------------
+// Copyrights (c) 2016 LeafyYT-Downloader INC. All rights reserved.
+// -----------------------------------------------------------
+
+#region
+
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+#endregion
+
+namespace Leafy_MD5check.Classes
+{
+    internal sealed class SelectableViewModel : INotifyPropertyChanged
+    {
+        private string _name;
+        private string _path;
+        private string _result;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name == value) return;
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Path
+        {
+            get { return _path; }
+            set
+            {
+                if (_path == value) return;
+                _path = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Result
+        {
+            get { return _result; }
+            set
+            {
+                if (_result == value) return;
+                _result = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            handler?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
